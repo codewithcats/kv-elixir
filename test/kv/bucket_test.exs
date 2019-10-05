575db1cfbc1ed9e9d349a93cc712846bbd6adf13
Tanawat Tassana <amp.tanawat@gmail.com>
2019-10-05 20:47:42 +0700

    Add delete function

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -12,4 +12,11 @@ defmodule KV.BucketTest do
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
   end
+
+  test "delete value by key", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "milk", 3)
+    value = KV.Bucket.delete(bucket, "milk")
+    assert 3 = value
+    assert KV.Bucket.get(bucket, "milk") |> is_nil
+  end
 end
